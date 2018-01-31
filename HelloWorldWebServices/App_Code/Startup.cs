4d39818eba1209739bf3155a58d212517c085e3c
Michael Brown <michaelfordbrown@hotmail.com>
2018-01-31 14:33:34 +0000

    Add project files.

---
+++ HelloWorldWebServices/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloWorldWebServices.Startup))]
+namespace HelloWorldWebServices
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
